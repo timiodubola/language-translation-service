0a7c314785191e5de96b7e2798326fb1e9a3af59
timiodubola <odubolaoluwatimilehin@gmail.com>
2024-09-03 20:12:44 +0100

    feat: basic express setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    console.log('[GET ROUTE]');
+    res.send('HELLO FROM HOMEPAGE');
+})
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
